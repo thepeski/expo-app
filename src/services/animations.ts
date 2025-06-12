06d384790d54b43fd8e371807d807ba210cbfabb
thepeski <jpeski@icloud.com>
2025-06-12 14:53:32 +0200

    feat(src/services): add bounce & unify animations under single name

---
+++ src/services/animations.ts
@@ -0,0 +1,47 @@
+/* @services/headerAnimations.ts */
+// react animted animations
+
+// react imports
+import { Animated } from "react-native";
+import { SharedValue, withSequence, withSpring } from "react-native-reanimated";
+
+const animations = {
+    headers: {
+        fadeIn: (x: Animated.Value, inputRange = { min: 0, max: 1 }) => {
+            return {
+                opacity: x.interpolate({
+                    inputRange: [inputRange.min, inputRange.max],
+                    outputRange: [0, 1], // opacity from 0% to 100%
+                    extrapolate: "clamp"
+                })
+            };
+        },
+        fadeOut: (x: Animated.Value, inputRange = { min: 0, max: 1 }) => {
+            return {
+                opacity: x.interpolate({
+                    inputRange: [inputRange.min, inputRange.max],
+                    outputRange: [1, 0], // opacity from 100% to 0%
+                    extrapolate: "clamp"
+                })
+            };
+        }
+    },
+    scale: {
+        bounce: (
+            scale: SharedValue<number>,
+            min: number,
+            max: number,
+            options?: { damping?: number, stiffness?: number }
+        ) => {
+            scale.value = withSequence(
+                withSpring(
+                    max ?? 1.2,
+                    { damping: options?.damping ?? 8, stiffness: options?.stiffness ?? 500 }
+                ),
+                withSpring(min ?? 1)
+            )
+        }
+    }
+}
+
+export default animations;
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
 /* @services/index.ts */
 
-export { default as headerAnimations } from "./headerAnimations";
+export { default as animations } from "./animations";
 export * from "./storage";
