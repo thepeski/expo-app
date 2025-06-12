ed37a72a830f5b4b1ea31f582cc35ab1c3a0574c
thepeski <jpeski@icloud.com>
2025-06-12 12:57:27 +0200

    chore: add views alias

--- babel.config.js
+++ babel.config.js
@@ -19,6 +19,7 @@ module.exports = function (api) {
                         "@screens": "./src/screens",
                         "@services": "./src/services",
                         "@utils": "./src/utils",
+                        "@views": "./src/views",
                     },
                 },
             ],
