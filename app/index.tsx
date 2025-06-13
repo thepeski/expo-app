548520cf06141f280e242be1e030a890f664094c
thepeski <jpeski@icloud.com>
2025-06-13 13:17:46 +0200

    feat(app): add root & index layouts

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+/* index.tsx */
+// rendered by root layout
+
+// react imports
+import { SafeAreaView, Text } from "react-native";
+
+function Index() {
+    return (
+        <SafeAreaView>
+            <Text>Hello, world!</Text>
+        </SafeAreaView>
+    );
+}
+
+export default Index;
