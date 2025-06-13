c89a1c4d544efa37f0c8d1519cb58142867d6fbe
thepeski <jpeski@icloud.com>
2025-06-13 17:02:45 +0200

    feat(src/hooks): add bootstrap

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@
 
 export * from "./useAuth";
 export { default as useAuthRedirect } from "./useAuthRedirect";
+export { default as useBootstrap } from "./useBootstrap";
---
+++ src/hooks/useBootstrap.ts
@@ -0,0 +1,78 @@
+/* @hooks/useBootstrap.ts */
+// run tasks on startup
+
+// react imports
+import { useState, useEffect } from "react";
+
+// src imports
+import { Logger } from "@dev";
+import { loadFonts } from "@utils";
+
+// local imports
+import { useAuth } from "./useAuth";
+
+// imitate startup task
+async function startupTask() {
+
+    // success
+    await new Promise(result => setTimeout(result, 1000));
+
+    // crash
+    // await new Promise((_, reject) => {
+    //     setTimeout(() => {
+    //         reject(new Error("intentional startup crash"));
+    //     }, 1000);
+    // })
+}
+
+function useBootstrap() {
+    const logger = new Logger("useBootstrap.ts");
+
+    // initialise startup tasks status
+    const [isEffectLoading, setIsEffectLoading] = useState(true);
+    const [effectError, setEffectError] = useState(false);
+
+    // add other hooks here
+    const { isAuthLoading, authError } = useAuth();
+
+    // loading indocators
+    const isBootLoading = isEffectLoading || isAuthLoading;
+
+    // error indicators
+    const bootError = effectError || authError;
+
+    // master check
+    const bootReady = !isBootLoading && !bootError;
+
+    // log auth hooks status
+    useEffect(() => {
+        if (!isAuthLoading) logger.info("auth tasks completed");
+    }, [isAuthLoading]);
+
+    // run startup tasks
+    useEffect(() => {
+        void (async () => {
+            try {
+
+                // run startup tasks
+                await startupTask();
+                await loadFonts();
+
+                // boot successful
+                setIsEffectLoading(false);
+
+                logger.info("startup tasks completed");
+            } catch (e) {
+                const error = e instanceof Error ? e.message : String(e);
+                setEffectError(true);
+
+                logger.error(error);
+            }
+        })();
+    }, []);
+
+    // resolve
+    return { bootReady, isBootLoading, bootError };
+}
+
+export default useBootstrap;
