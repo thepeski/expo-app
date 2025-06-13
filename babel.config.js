e47c63133aee67aeba5ab62720aeaa294bfccc6f
thepeski <jpeski@icloud.com>
2025-06-13 13:25:29 +0200

    chore: create babel config & add dev alias

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+/* babel.config.js */
+
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ["babel-preset-expo"],
+        plugins: [
+            [
+                "module-resolver",
+                {
+                    extensions: [".js", ".jsx", ".ts", ".tsx"],
+                    alias: {
+                        "@dev": "./src/dev",
+                    },
+                },
+            ],
+        ],
+    };
+};
