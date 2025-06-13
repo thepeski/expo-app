e5859bb1a67ab462915bd02fe4a201b4d72c4f59
thepeski <jpeski@icloud.com>
2025-06-13 17:05:27 +0200

    feat(src/hooks): add image preloader

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,4 @@
 export * from "./useAuth";
 export { default as useAuthRedirect } from "./useAuthRedirect";
 export { default as useBootstrap } from "./useBootstrap";
+export { default as useImages } from "./useImages";
