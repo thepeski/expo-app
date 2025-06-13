946238ce5905237bfafd941568fd214b7b5afc88
thepeski <jpeski@icloud.com>
2025-06-13 13:56:46 +0200

    chore: add fonts alias

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = function (api) {
                     extensions: [".js", ".jsx", ".ts", ".tsx"],
                     alias: {
                         "@dev": "./src/dev",
+                        "@fonts": "./assets/fonts",
                         "@utils": "./src/utils",
                     },
                 },
