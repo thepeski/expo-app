52f4256fa16ea0fcc3f1d2765c5b9ad345d589b3
thepeski <jpeski@icloud.com>
2025-06-13 17:09:08 +0200

    build: configure images

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,6 @@
 
 const { getDefaultConfig } = require("expo/metro-config");
 const config = getDefaultConfig(__dirname);
+config.resolver.assetExts.push("png");
 config.resolver.unstable_enablePackageExports = false;
 module.exports = config;
