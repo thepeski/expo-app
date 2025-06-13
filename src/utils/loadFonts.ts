6c0f216a2c8f31972da22a01143b5b34bb74c559
thepeski <jpeski@icloud.com>
2025-06-13 16:58:16 +0200

    refactor(src/utils): make font loader asynchronous

--- src/utils/loadFonts.ts
+++ src/utils/loadFonts.ts
@@ -7,30 +7,28 @@ import { loadAsync } from "expo-font";
 // src imports
 import { Logger } from "@dev";
 
-function loadFonts() {
+async function loadFonts() {
     const logger = new Logger("loadFonts.ts");
 
-    void (async () => {
-        try {
-            await loadAsync({
-                lato: require("@fonts/latoRegular.ttf"),
-                latoItalic: require("@fonts/latoItalic.ttf"),
-                latoBold: require("@fonts/latoBold.ttf"),
-                latoBoldItalic: require("@fonts/latoBoldItalic.ttf"),
-                latoBlack: require("@fonts/latoBlack.ttf"),
-                latoBlackItalic: require("@fonts/latoBlackItalic.ttf")
-            });
+    try {
+        await loadAsync({
+            lato: require("@fonts/latoRegular.ttf"),
+            latoItalic: require("@fonts/latoItalic.ttf"),
+            latoBold: require("@fonts/latoBold.ttf"),
+            latoBoldItalic: require("@fonts/latoBoldItalic.ttf"),
+            latoBlack: require("@fonts/latoBlack.ttf"),
+            latoBlackItalic: require("@fonts/latoBlackItalic.ttf")
+        });
 
-            logger.info("fonts loaded");
-        } catch (e) {
-            const error = e instanceof Error ? e.message : String(e);
+        logger.info("fonts loaded");
+    } catch (e) {
+        const error = e instanceof Error ? e.message : String(e);
 
-            logger.error(error);
+        logger.error(error);
 
-            // rethrow for caller
-            throw error;
-        }
-    })();
+        // rethrow for caller
+        throw error;
+    }
 }
 
 export default loadFonts;
