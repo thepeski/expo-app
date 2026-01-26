b60638ae4bdd63206434a053ebaf9e9a07a30d2b
thepeski <jpeski@icloud.com>
2026-01-26 15:27:04 +0100

    refactor: embed bootstrap in root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,12 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 // expo
 import { Slot } from "expo-router";
 
+// src
+import { useBoot } from "@hooks";
+
 function Root() {
+    useBoot();
+
     return (
         <SafeAreaProvider>
             <Slot />
