766c2a49aea60d99c28efd371f5b87a69dff1ac2
thepeski <jpeski@icloud.com>
2026-01-26 14:07:52 +0100

    feat: create radius presets

---
+++ src/constants/radius/index.ts
@@ -0,0 +1,3 @@
+/* @constants/radius */
+
+export { radius } from "./radius";
---
+++ src/constants/radius/radius.ts
@@ -0,0 +1,6 @@
+/* @constants/radius/radius.ts */
+// radius presets
+
+const radius = { none: 0, xs: 4, sm: 6, md: 12, lg: 16, xl: 24, xxl: 32, pill: 999 } as const;
+
+export { radius };
