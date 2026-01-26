eb3672b359183d25b0db32ce8343f6cc61246522
thepeski <jpeski@icloud.com>
2026-01-26 19:12:29 +0100

    refactor: update size presets

--- src/constants/sizes/sizes.ts
+++ src/constants/sizes/sizes.ts
@@ -1,6 +1,6 @@
 /* @constants/sizes/sizes.ts */
 // sizes presets
 
-const sizes = { none: 0, xs: 12, sm: 10, md: 20, lg: 24, xl: 32, xxl: 40, xxxl: 48 } as const;
+const sizes = { none: 0, xs: 10, sm: 14, md: 20, lg: 24, xl: 32, xxl: 40, xxxl: 48 } as const;
 
 export { sizes };
